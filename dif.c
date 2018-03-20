bdd624500834a5f6d884ba658459fe234141d36b
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-03-20 15:55:41 +0530

    Create dif.c

---
+++ dif.c
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f,g,h,i;
+scanf("%d%d",&a,&b);
+scanf("%d%d",&c,&d);
+scanf("%d%d",&e,&f);
+g=a-b;
+h=c-d;
+i=e-f;
+printf("%d\n",g);
+printf("%d\n",h);
+printf("%d\n",i);
+getch();
+}
