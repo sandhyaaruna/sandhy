218ba8d39b4110fd476ae162a6ec08f978d29bfe
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-03-20 16:25:01 +0530

    Create mod.h

---
+++ mod.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long int a,b,c,d,e,f,g,h,i,j,k,l;
+scanf("%ld%ld",&a,&b);
+scanf("%ld%ld",&c,&d);
+scanf("%ld%ld",&e,&f);
+scanf("%ld%ld",&g,&h);
+i=(a/b);
+j=(c%d);
+k=(e/f);
+l=(g%h);
+printf("%ld\n",i);
+printf("%ld\n",j);
+printf("%ld\n",k);
+printf("%ld\n",l);
+getch();
+}
