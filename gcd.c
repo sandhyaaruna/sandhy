d0bfc1580c0c03b7fb803ee8e2914d0b4880a06d
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-03-20 16:40:03 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+scanf("%d%d",&a,&b);
+for(i=i:i<=a && i<=b:++i)
+{
+if(a%i==0 && b%i==0)
+{
+g=i
+}
+}
+printf("%d",g);
+getch();
+}
